2f5be479787727e8db92c7f6188effde1e5cbe11
Jose Luis Lopez Bellido <joseluislobell@gmail.com>
2023-01-18 08:32:03 +0100

    Add Ninja Logo to AuthStack.navigator header

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 The aim of this repository is to gather, with educational purposes, the step by step implementation
 for a complete authentication workflow in a React Native App.
+
+## Images property
+
+Images listed below are property of ***[The Ninja Project]*** & ***BBVA 2023***, they kindly allowed its use for this Wokshop to be possible:
+* [ninjaLogo]
+
+
+[The Ninja Project]: https://www.theninjaproject.bbva/
+[ninjaLogo]: assets/ninjaLogo.png
--- src/screens/auth/AuthNavigator.js
+++ src/screens/auth/AuthNavigator.js
@@ -1,3 +1,4 @@
+import { Image } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import Login from './Login'
@@ -5,9 +6,19 @@ import Signup from './Signup'
 
 const AuthStack = createNativeStackNavigator()
 
+const headerTitle = (props) => (
+    <Image
+        style={{ width: 110, height: 50 }}
+        resizeMode="contain"
+        source={require('../../../assets/ninjaLogo.png')}
+    />
+)
+
 const AuthNavigator = ({ screenOptions }) => {
     return (
-        <AuthStack.Navigator screenOptions={screenOptions}>
+        <AuthStack.Navigator
+            screenOptions={{ ...screenOptions, headerTitle: headerTitle }}
+        >
             <AuthStack.Screen name="Login" component={Login} />
             <AuthStack.Screen name="Signup" component={Signup} />
         </AuthStack.Navigator>
