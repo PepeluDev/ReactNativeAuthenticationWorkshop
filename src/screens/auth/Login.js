1459998e685ee0e81185656366b4b0b67c293dae
Jose Luis Lopez Bellido <joseluislobell@gmail.com>
2023-01-24 09:35:38 +0100

    Add navigation buttons between Login and Signup screens

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,17 +1,27 @@
 import { StyleSheet, View } from 'react-native'
 
-import WithImageBackground from '../helpers/WithImageBackground'
 import AuthForm from '../../components/Auth/AuthForm'
+import FlatButton from '../../components/ui/FlatButton'
+import WithImageBackground from '../helpers/WithImageBackground'
 
 const Login = ({ navigation }) => {
     function loginHandler(loginInfo) {
         console.log(JSON.stringify(loginInfo))
     }
 
+    function goToSignUpHandler() {
+        navigation.replace('Signup')
+    }
+
     return (
         <WithImageBackground>
             <View style={styles.rootContainer}>
                 <AuthForm isLogin={true} onSubmit={loginHandler} />
+                <View style={styles.buttonContainer}>
+                    <FlatButton onPress={goToSignUpHandler}>
+                        Create a new account
+                    </FlatButton>
+                </View>
             </View>
         </WithImageBackground>
     )
@@ -21,6 +31,9 @@ const styles = StyleSheet.create({
     rootContainer: {
         flex: 1,
         justifyContent: 'center',
+    },
+    buttonContainer: {
+        marginVertical: 3,
     },
 })
 export default Login
--- src/screens/auth/Signup.js
+++ src/screens/auth/Signup.js
@@ -1,17 +1,27 @@
 import { StyleSheet, View } from 'react-native'
 
-import WithImageBackground from '../helpers/WithImageBackground'
 import AuthForm from '../../components/Auth/AuthForm'
+import FlatButton from '../../components/ui/FlatButton'
+import WithImageBackground from '../helpers/WithImageBackground'
 
-const Signup = () => {
+const Signup = ({ navigation }) => {
     function signupHandler(loginInfo) {
         console.log(JSON.stringify(loginInfo))
+    }
+
+    function goToLogInUpHandler() {
+        navigation.replace('Login')
     }
 
     return (
         <WithImageBackground>
             <View style={styles.rootContainer}>
                 <AuthForm isLogin={false} onSubmit={signupHandler} />
+                <View style={styles.buttonContainer}>
+                    <FlatButton onPress={goToLogInUpHandler}>
+                        I already have account, login
+                    </FlatButton>
+                </View>
             </View>
         </WithImageBackground>
     )
@@ -21,6 +31,9 @@ const styles = StyleSheet.create({
     rootContainer: {
         flex: 1,
         justifyContent: 'center',
+    },
+    buttonContainer: {
+        marginVertical: 3,
     },
 })
 export default Signup
